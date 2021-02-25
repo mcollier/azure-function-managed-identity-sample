95135bd8998f3a1b994f916bf7dff3d4a1e490d0
Michael Collier <mcollier@microsoft.com>
2021-02-24 21:44:39 -0500

    Removed unnecessary using statements.

--- src/SampleFunction.cs
+++ src/SampleFunction.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-//using Microsoft.Azure.EventHubs;
 using Azure.Messaging.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
